b0980fee7b998c057553bf3264c2b559f071f7ff
Natsumi.H <natsmy.1211@gmail.com>
2023-04-17 12:17:19 +0800

    remove test code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home = () => {
       <section className="showcase">
         <div className="dark-overlay">
           <div className="showcase-inner container">
-            <h1 className="display-4">Find a Code Bootcamp test</h1>
+            <h1 className="display-4">Find a Code Bootcamp</h1>
             <p className="lead">
               Find, rate and read reviews on coding bootcamps
             </p>
